047908f8b34899665b3dd979b4c0f58ecf08ef45
roma_marshall <roma_marshall@outlook.com>
2024-09-07 15:35:05 +0200

    added env

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,12 @@
 </template>
 
 <script setup>
-import { Bot } from 'grammy';
+import { Bot } from 'grammy'
 
-// Create an instance of the `Bot` class and pass your bot token to it.
-const bot = new Bot('process.env.API_KEY'); // <-- put your bot token between the ''
+const bot = new Bot(process.env.API_KEY)
 
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
+bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
+bot.on('message', (ctx) => ctx.reply('Got another message!'))
 
-// Handle the /start command.
-bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
-// Handle other messages.
-bot.on('message', (ctx) => ctx.reply('Got another message!'));
-
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
-
-// Start the bot.
-bot.start();
+bot.start()
 </script>
