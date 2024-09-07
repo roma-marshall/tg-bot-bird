78afef815e76a9b460c80e9af54c36c6d15aea96
roma_marshall <roma_marshall@outlook.com>
2024-09-07 15:42:56 +0200

    added vite api key

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 <script setup>
 import { Bot } from 'grammy'
 
-const bot = new Bot(process.env.API_KEY)
+const bot = new Bot(import.meta.env.VITE_API_KEY)
 
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
 bot.on('message', (ctx) => ctx.reply('Got another message!'))
