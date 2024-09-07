f9d8ece3be2132e6490d7cac731f2e43d1a75a2e
roma_marshall <roma_marshall@outlook.com>
2024-09-07 17:10:32 +0200

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
 <script setup>
 import { Bot } from 'grammy'
 
-// const api_key = import.meta.env.VITE_API_KEY
-const api_key = process.env.VITE_API_KEY
+// const api_key = import.meta.env.VITE_API_KEY // local
+const api_key = process.env.VITE_API_KEY // vercel
 
 const bot = new Bot(api_key, {
   headers: {
@@ -16,8 +16,17 @@ const bot = new Bot(api_key, {
   }
 })
 
-bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
-bot.on('message', (ctx) => ctx.reply('Got another message!'))
+bot.command('start', async (ctx) => {
+  await ctx.reply('Welcome to Flappy Bird! 🎉🎉🎉\n' +
+      '\n' +
+      'Play a decentralized web3 decentralized Telegram game here, with the old all familiar classic gameplay. We have a unique vision to launch new Telegram Mini Apps.\n' +
+      '\n' +
+      'Start playing now and who knows what cool stuff you might find! 🚀\n' +
+      '\n' +
+      'Stay with Flappy Bird! 🌟')
+})
+
+// bot.on('message', (ctx) => ctx.reply('Got another message!'))
 
 bot.start()
 </script>
