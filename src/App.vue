1b2207f8780e639ad6300f18b0a9abf42a50c916
roma_marshall <roma_marshall@outlook.com>
2024-09-07 15:55:40 +0200

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,15 @@
 <script setup>
 import { Bot } from 'grammy'
 
-const bot = new Bot(process.env.API_KEY)
+console.log(process.env.ENV_VAR)
 
-bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
-bot.on('message', (ctx) => ctx.reply('Got another message!'))
 
-bot.start()
+// const bot = new Bot(import.meta.env.VITE_API_KEY)
+
+// const bot = new Bot(process.env.API_KEY)
+//
+// bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
+// bot.on('message', (ctx) => ctx.reply('Got another message!'))
+//
+// bot.start()
 </script>
